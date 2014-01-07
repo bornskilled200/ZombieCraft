6e1048ed75e39b445b39247ff814bcd4e0706d13
David Park <bornskilled200@gmail.com>
2014-01-06 23:38:28 -0500

    First Commit

---
+++ src/craft/Unit.java
@@ -0,0 +1,47 @@
+package craft;
+
+/**
+ * Created by David Park on 1/6/14.
+ */
+public strictfp abstract class Unit {
+    private int health;
+    private  float x;
+    private float y;
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setPosition(float x, float y)
+    {
+        this.x=x;
+        this.y=y;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public boolean isDead()
+    {
+        return getHealth()<=0;
+    }
+
+    public abstract void act();
+}
