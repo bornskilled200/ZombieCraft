0d1731bd7034b7f31ae7baf9143c3d459ad46878
David <bornskilled020@gmail.com>
2014-01-30 10:42:39 -0500

    Basic Boundary checking and pushback, Debug System.out are still in the code because i want to clean up the code in general and do not want to break anything

--- src/zombiecraft/screen/GameMap.java
+++ src/zombiecraft/screen/GameMap.java
@@ -1,5 +1,6 @@
 package zombiecraft.screen;
 
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.FPSLogger;
@@ -9,12 +10,13 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Interpolation;
-import zombiecraft.human.Human;
-import zombiecraft.unit.MainBuilding;
+import com.badlogic.gdx.math.Vector2;
 import zombiecraft.Player;
 import zombiecraft.Unit;
 import zombiecraft.ZombieCraft;
 import zombiecraft.human.Base;
+import zombiecraft.human.Human;
+import zombiecraft.unit.MainBuilding;
 import zombiecraft.unit.MovableUnit;
 import zombiecraft.zombie.Brain;
 
@@ -23,25 +25,32 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 /**
  * Created by David Park on 1/7/14.
  */
-public class GameMap implements Screen {
+public class GameMap implements Screen
+{
     private final SpriteBatch spriteBatch;
     private final BitmapFont bitmapFont;
+    private final float UPDATES_PER_SECOND = 25;
+    private final float SECONDS_PER_UPDATE = 1 / UPDATES_PER_SECOND;
+    private final int MAX_FRAME_SKIP = 5;
+    public float accumalator;
+    FPSLogger fpsLogger = new FPSLogger();
     private OrthographicCamera camera;
-
     private Map<String, Texture> textures;
-
     private ZombieCraft zombieCraft;
     private List<Player> players;
     private Map<Player, MainBuilding> mainBuildingMap;
     private List<Unit> mainBuildings;
     private List<Unit> units;
     private Map<Unit, Player> playerMap;
+    private int time;
 
 
-    public GameMap(ZombieCraft zombieCraft, List<Player> players) {
+    public GameMap(ZombieCraft zombieCraft, List<Player> players)
+    {
         this.spriteBatch = zombieCraft.getSpriteBatch();
         this.bitmapFont = zombieCraft.getBitmapFont();
         this.zombieCraft = zombieCraft;
@@ -54,7 +63,8 @@ public class GameMap implements Screen {
         mainBuildingMap = new HashMap<Player, MainBuilding>();
         camera = new OrthographicCamera();
 
-        for (Player player : players) {
+        for (Player player : players)
+        {
             MainBuilding mainBuilding = player.race.getMainBuilding();
             mainBuilding.setPosition((float) Math.random() * 480, (float) Math.random() * 480);
             mainBuildings.add(mainBuilding);
@@ -69,20 +79,14 @@ public class GameMap implements Screen {
         Gdx.gl.glClearColor(1, 1, 1, 0);
     }
 
-    public float accumalator;
-    private final float UPDATES_PER_SECOND = 25;
-    private final float SECONDS_PER_UPDATE = 1 / UPDATES_PER_SECOND;
-    private final int MAX_FRAME_SKIP = 5;
-    private int time;
-
-
-    FPSLogger fpsLogger = new FPSLogger();
     @Override
-    public void render(float delta) {
+    public void render(float delta)
+    {
         accumalator += delta;
 
         int loops = 0;
-        while (accumalator > SECONDS_PER_UPDATE && loops < MAX_FRAME_SKIP) {
+        while (accumalator > SECONDS_PER_UPDATE && loops < MAX_FRAME_SKIP)
+        {
             update_game(time);
 
             accumalator -= SECONDS_PER_UPDATE;
@@ -92,16 +96,18 @@ public class GameMap implements Screen {
 
         float interpolation = accumalator / SECONDS_PER_UPDATE;
         display_game(interpolation);
-        fpsLogger.log();
+        //fpsLogger.log();
     }
 
-    public void addUnit(Player player, Unit unit) {
+    public void addUnit(Player player, Unit unit)
+    {
         if (unit instanceof MainBuilding)
             throw new IllegalArgumentException("Why are you adding a Main Building, stop that");
         if (players.contains(player) == false)
             throw new IllegalArgumentException("This player does not exist in this map");
 
-        if (unit instanceof MovableUnit) {
+        if (unit instanceof MovableUnit)
+        {
             MovableUnit movableUnit = (MovableUnit) unit;
             movableUnit.setTime(time);
         }
@@ -109,86 +115,158 @@ public class GameMap implements Screen {
         playerMap.put(unit, player);
     }
 
-    private void display_game(float interpolation) {
+    private void display_game(float interpolation)
+    {
 
         Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 
+
         spriteBatch.begin();
-        for (Unit unit : units) {
+        for (Unit unit : units)
+        {
             float x = unit.getX();
             float y = unit.getY();
-            if (unit instanceof MovableUnit) {
+            if (unit instanceof MovableUnit)
+            {
                 MovableUnit movableUnit = (MovableUnit) unit;
-                x = Interpolation.linear.apply(movableUnit.getPreviousX(), x, interpolation);//previousX + (x- previousX)*interpolation;
-                y = Interpolation.linear.apply(movableUnit.getPreviousY(), y, interpolation);//previousY + (y- previousY)*interpolation;
-                float velocityX = movableUnit.getVelocityX();
-                float velocityY = movableUnit.getVelocityY();
-                if (velocityX !=0 && velocityY !=0)
-                {
-                    float max = Math.min(x / (-velocityX), y / (-velocityY));
-                    float ghostX = x - (max * -velocityX);
-                    float ghostY = y - (max * -velocityY);
-                    spriteBatch.setColor(1,1,1,.5f);
-                    spriteBatch.draw(textures.get(unit.getName()),ghostX,ghostY);
+                x = Interpolation.linear.apply(movableUnit.getPreviousX(), x,
+                                               interpolation);//previousX + (x- previousX)*interpolation;
+                y = Interpolation.linear.apply(movableUnit.getPreviousY(), y,
+                                               interpolation);//previousY + (y- previousY)*interpolation;
 
-                    spriteBatch.setColor(1,1,1,1);
-                }
             }
             spriteBatch.draw(textures.get(unit.getName()), x, y);
         }
         spriteBatch.end();
     }
 
-    private void update_game(int time) {
-        for (Player player : players) {
+    private void update_game(int time)
+    {
+        for (Player player : players)
+        {
             player.poll(this, mainBuildingMap.get(player));
         }
 
-        for (Unit unit : units) {
+        for (Unit unit : units)
+        {
             unit.act(time);
+            if (unit instanceof MovableUnit)
+            {
+                MovableUnit movableUnit = (MovableUnit) unit;
+                float velocityX = movableUnit.getVelocityX();
+                float velocityY = movableUnit.getVelocityY();
+                float x = movableUnit.getX();
+                float y = movableUnit.getY();
+                if ((velocityX != 0 && velocityY != 0) && ((x < 0 || x > 640 - 64) || (y < 0 || y > 480 - 64)))
+                {
+                    float f1 = x / (-velocityX);
+                    float f2 = y / (-velocityY);
+                    float max = Math.max(f1, f2);
+                    if (f1>0)
+                        max = f2;
+                    else if (f2>0)
+                        max = f1;
+                    float f3 = (x - 640 + 64) / (-velocityX);
+                    float f4 = (y - 480 + 64) / (-velocityY);
+                    float min = Math.max(f3, f4);
+
+                    if (f3>0)
+                        min = f4;
+                    else if (f4>0)
+                        min = f3;
+                    float back = Math.max(max, min);
+                    if (back <= 0)
+                    {
+                        float max1 = Math.max(1, Math.min(640 - 64 - 1, x - (back * -velocityX)));
+                        float max2 = Math.max(1, Math.min(480 - 64 - 1, y - (back * -velocityY)));
+
+                        float len = new Vector2(max1 - x, max2 - y).len();
+                        if (len > 7)
+                        {
+                            System.out.println(len);
+                            System.out.println(x + ", " + y);
+                            System.out.println(max1 + ", " + max2);
+                            System.out.println("back is " + back);
+                            System.out.println(f1 + "\t" + (Math.max(1, Math.min(640 - 64 - 1, x - (f1 * -velocityX)))) + ", " +
+                                               (Math.max(1, Math.min(640 - 64 - 1, y - (f1 * -velocityY)))));
+                            System.out.println(f2 + "\t" + (Math.max(1, Math.min(640 - 64 - 1, x - (f2 * -velocityX)))) + ", " +
+                                               (Math.max(1, Math.min(640 - 64 - 1, y - (f2 * -velocityY)))));
+                            System.out.println(f3 + "\t" + (Math.max(1, Math.min(640 - 64 - 1, x - (f3 * -velocityX)))) + ", " +
+                                               (Math.max(1, Math.min(640 - 64 - 1, y - (f3 * -velocityY)))));
+                            System.out.println(f4 + "\t" + (Math.max(1, Math.min(640 - 64 - 1, x - (f4 * -velocityX)))) + ", " +
+                                               (Math.max(1, Math.min(640 - 64 - 1, y - (f4 * -velocityY)))));
+                            System.out.println();
+                        }
+                        unit.setX(max1);
+                        unit.setY(max2);
+                    }  else {
+                        System.out.println("SOMETHING IS WRONG");
+                        System.out.println(x + ", " + y);
+                        System.out.println("back is " + back);
+                        System.out.println(f1 + "\t" + (Math.max(1, Math.min(640 - 64 - 1, x - (f1 * -velocityX)))) + ", " +
+                                           (Math.max(1, Math.min(640 - 64 - 1, y - (f1 * -velocityY)))));
+                        System.out.println(f2 + "\t" + (Math.max(1, Math.min(640 - 64 - 1, x - (f2 * -velocityX)))) + ", " +
+                                           (Math.max(1, Math.min(640 - 64 - 1, y - (f2 * -velocityY)))));
+                        System.out.println(f3 + "\t" + (Math.max(1, Math.min(640 - 64 - 1, x - (f3 * -velocityX)))) + ", " +
+                                           (Math.max(1, Math.min(640 - 64 - 1, y - (f3 * -velocityY)))));
+                        System.out.println(f4 + "\t" + (Math.max(1, Math.min(640 - 64 - 1, x - (f4 * -velocityX)))) + ", " +
+                                           (Math.max(1, Math.min(640 - 64 - 1, y - (f4 * -velocityY)))));
+                        System.out.println();
+                    }
+                }
+            }
         }
 
-        for (int i = units.size() - 1; i >= 0; i--) {
+        for (int i = units.size() - 1; i >= 0; i--)
+        {
             Unit unit = units.get(i);
 
-            if (!unit.isDead()) continue;
+            if (!unit.isDead())
+                continue;
 
             units.remove(i);
-            if (unit instanceof MainBuilding && mainBuildings.remove(unit) && mainBuildings.size() == 1) {
+            if (unit instanceof MainBuilding && mainBuildings.remove(unit) && mainBuildings.size() == 1)
+            {
                 zombieCraft.setScreen(zombieCraft.getMenu());
             }
         }
     }
 
     @Override
-    public void resize(int width, int height) {
+    public void resize(int width, int height)
+    {
         camera.setToOrtho(true, width, height);
         camera.update();
         spriteBatch.setProjectionMatrix(camera.combined);
     }
 
     @Override
-    public void show() {
+    public void show()
+    {
 
     }
 
     @Override
-    public void hide() {
+    public void hide()
+    {
 
     }
 
     @Override
-    public void pause() {
+    public void pause()
+    {
 
     }
 
     @Override
-    public void resume() {
+    public void resume()
+    {
 
     }
 
     @Override
-    public void dispose() {
+    public void dispose()
+    {
 
     }
 }
